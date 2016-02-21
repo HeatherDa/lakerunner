4378eb43a735d6b6867ff15ff2453ac89fccce81
HeatherDa <crystal_tree@hotmail.com>
2016-02-21 02:51:40 -0600

    Finished

--- Main.java
+++ Main.java
@@ -4,28 +4,29 @@ public class Main {
 static Scanner scan;
 static Scanner scandoub;
     public static void main(String[] args) {
-        // receive lake and duration of run from user and calculate the fastest time for each lake in turn.
-        //arrange as hash of arrays.  Key to hash is lake name, array's contain times.
+        // receive input for lake and duration of run from user and calculate the fastest time for each lake in turn.
         scan = new Scanner(System.in);
         scandoub = new Scanner(System.in);
         HashMap<String, ArrayList<Double>> lakes = makelakes();
-        //System.out.println(lakes);
         for(String lake:lakes.keySet()){
             Double min=minvalue(lakes.get(lake));
-            System.out.println("The shortest time for lake "+lake+" was "+min+".");
+            System.out.println("The shortest time for lake "+lake+" was "+min+" minutes.");
         }
-
     }
     private static HashMap<String, ArrayList<Double>>makelakes(){
         HashMap<String,ArrayList<Double>>lakes= new HashMap<>();
-        System.out.println("How many entries do you wish to make?");
-        Double number = scandoub.nextDouble();
-        for(int i=0;i<number;i++) {
+        String lake="Hit Enter and extra time to finish inputting data.";
+        System.out.println(lake);
+
+        while(!lake.equals("")) {
 
             ArrayList<Double> times = new ArrayList<>();
             System.out.println("Please enter the name of the lake.");
-            String lake = scan.nextLine();
+            lake = scan.nextLine();
             lake=lake.toLowerCase();
+            if (lake.equals("")) {//test for end of data input
+                break;
+            }
             System.out.println("Please enter the time for the run.");
             double time = scandoub.nextDouble();
 
@@ -37,15 +38,16 @@ static Scanner scandoub;
                 times.add(time);
                 lakes.put(lake, times);
             }
-            //System.out.println("What is in my hash? " + lakes + " What is in my array? " + times);
         }
+        scan.close();
+        scandoub.close();
         return lakes;
     }
     private static double minvalue(ArrayList<Double>times){
         double minimum=1440;//number of minutes in a day.  Certainly longer than a run!
-        for(double time:times){
-            if(time<minimum){
-                minimum=time;
+        for(double time:times){//access entry in array
+            if(time<minimum){ //test entry to see if less than minimum
+                minimum=time; //update minimum
             }
         }
         return minimum;
